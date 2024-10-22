a69d916a43894b1c7ede867874cf5db55354d3ff
LebohangMashinini <palesal613@gmail.com>
2024-10-22 23:16:21 +0200

    fetched the data from the open weather api

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -7,7 +7,15 @@ API_KEY = os.getenv("API_KEY")
 
 def get_data():
     api_key = API_KEY
-    api_url = "https://home.openweathermap.org/data/2.5/weather?"
+    api_url = "http://api.openweathermap.org/data/2.5/weather?"
     city_name = "Johannesburg"
-
+    complete_url = api_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
     
+    # json method of response object 
+    # convert json format data into
+    # python format data
+    data = response.json()
+    print(data)
+
+get_data()
